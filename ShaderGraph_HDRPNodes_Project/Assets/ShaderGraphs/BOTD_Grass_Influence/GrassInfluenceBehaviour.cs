11a70f0a3b038a2338f96a84aff977596d58569a
Andy Touch <andyt@unity3d.com>
2019-02-11 14:32:34 +0100

    Added Project (Unity 2019.1b2)
    Package Versions:
    HDRP - 5.3.1-preview

---
+++ ShaderGraph_HDRPNodes_Project/Assets/ShaderGraphs/BOTD_Grass_Influence/GrassInfluenceBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class GrassInfluenceBehaviour : MonoBehaviour
+{
+
+    public float influenceAmount;
+    public Color influenceAreaColor;
+
+    void Update()
+    {
+        Shader.SetGlobalVector("_Grass_Influence_Position", transform.position);
+        Shader.SetGlobalFloat("_Grass_Influence_Amount", -influenceAmount);
+        Shader.SetGlobalColor("_Grass_Influence_AreaColor", influenceAreaColor);
+    }
+}
